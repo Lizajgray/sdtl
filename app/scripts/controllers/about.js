5379f4de513e7b30a345dd227314e984147eff42
Liza Gray <lizajgray6590@gmail.com>
2015-01-13 19:04:31 -0800

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sdtlApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the sdtlApp
+ */
+angular.module('sdtlApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sdtlApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sdtlApp
+ */
+angular.module('sdtlApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
